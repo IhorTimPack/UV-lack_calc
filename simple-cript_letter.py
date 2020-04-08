cde4c040e6bcb48914b366bb7c232a0229efe00f
IhorTimPack <63108596+IhorTimPack@users.noreply.github.com>
2020-04-08 12:08:08 +0300

    Add files via upload

--- simple-cript_letter.py
+++ simple-cript_letter.py
@@ -1,15 +1,17 @@
 import simplecrypt
 crypt_file = open(r"D:\Python\encrypted.bin", "rb")
-password_file = open(r"D:\Python\passwords.crypt_text")
-crypt_text = crypt_file.read()
+password_file = open(r"D:\Python\passwords.txt")
+crypt_text = crypt_file.read().strip()
 print("crypt_text: ", crypt_text)
 for i in range(10):
     print(i)
-    passw = password_file.readline()
-    print("passw ", passw)
+    password = password_file.readline().strip()
+    print("password ", password)
     try:
-        enctxt = simplecrypt.decrypt(passw, crypt_text).decode('utf8')
-        print("ancwer:", enctxt)
+        enctxt = simplecrypt.decrypt(password, crypt_text).decode('utf8')
+        print("answer:", enctxt)
     except simplecrypt.DecryptionException:
         print("exception")
         continue
+crypt_file.close()
+password_file.close()
